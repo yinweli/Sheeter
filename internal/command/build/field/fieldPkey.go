81cd06ef1d31be41cef5de64a448010bca019f91
yinweli <yinweli@gmail.com>
2022-05-20 23:17:15 +0800

    新增主要索引

---
+++ internal/command/build/field/fieldPkey.go
@@ -0,0 +1,50 @@
+package field
+
+import "strconv"
+
+// Pkey 主要索引
+type Pkey struct {
+	Data
+}
+
+// StyleJson 取得json欄位類型
+func (this *Pkey) StyleJson() string {
+	return "pkey"
+}
+
+// StyleCpp 取得c++欄位類型
+func (this *Pkey) StyleCpp() string {
+	return "Sheet::pkey" // pkey型態宣告在命名空間Sheet中
+}
+
+// StyleCs 取得c#欄位類型
+func (this *Pkey) StyleCs() string {
+	return "int"
+}
+
+// StyleGo 取得go欄位類型
+func (this *Pkey) StyleGo() string {
+	return "int"
+}
+
+// Hide 是否隱藏
+func (this *Pkey) Hide() bool {
+	return false
+}
+
+// PrimaryKey 是否是主要索引
+func (this *Pkey) PrimaryKey() bool {
+	return true
+}
+
+// FillToMetas 寫入到元資料列表
+func (this *Pkey) FillToMetas(metas Metas, data string) error {
+	value, err := strconv.Atoi(data)
+
+	if err != nil {
+		return err
+	} // if
+
+	metas[this.Name] = value
+	return nil
+}
