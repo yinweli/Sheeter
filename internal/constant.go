99e0a3267e2082422073b40e6c4a3ae228ddbf9b
yilin.li@dena.com <yilin.li@dena.com>
2022-05-20 21:02:36 +0800

    移除不需要的程式碼

--- internal/constant.go
+++ internal/constant.go
@@ -1,5 +1,4 @@
 package internal
 
-const Title string = "sheeter"       // 程式名稱
-const Version string = "0.0.1"       // 版本字串
-const LogName string = "sheeter.log" // 日誌檔名
+const Title string = "sheeter" // 程式名稱
+const Version string = "0.0.1" // 版本字串
