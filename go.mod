05f625c749928258f186ac5454c48e8a885995d8
yilin.li@dena.com <yilin.li@dena.com>
2022-05-20 15:33:07 +0800

    新增單元測試

--- go.mod
+++ go.mod
@@ -8,6 +8,10 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 )
--- internal/command/buildConfig.go
+++ internal/command/buildConfig.go
@@ -14,18 +14,18 @@ type BuildConfig struct {
 // Check 檢查設定資料是否正確
 func (this *BuildConfig) Check() (result bool) {
 	checker := util.NewChecker()
-	checker.Add(this.Global.ExcelPath == "", "Global: excelPath empty")
-	checker.Add(this.Global.OutputPathJson == "", "Global: outputPathJson empty")
-	checker.Add(this.Global.OutputPathGo == "", "Global: outputPathGo empty")
-	checker.Add(this.Global.OutputPathCs == "", "Global: outputPathCs empty")
-	checker.Add(this.Global.OutputPathCpp == "", "Global: outputPathCpp empty")
-	checker.Add(this.Global.LineOfNote >= this.Global.LineOfData, "Global: line of note can't greater than line of data")
-	checker.Add(this.Global.LineOfField >= this.Global.LineOfData, "Global: line of field can't greater than line of data")
-	checker.Add(len(this.Elements) <= 0, "element: element empty")
+	checker.Add(this.Global.ExcelPath != "", "Global: excelPath empty")
+	checker.Add(this.Global.OutputPathJson != "", "Global: outputPathJson empty")
+	checker.Add(this.Global.OutputPathGo != "", "Global: outputPathGo empty")
+	checker.Add(this.Global.OutputPathCs != "", "Global: outputPathCs empty")
+	checker.Add(this.Global.OutputPathCpp != "", "Global: outputPathCpp empty")
+	checker.Add(this.Global.LineOfData > this.Global.LineOfNote, "Global: line of note can't greater than line of data")
+	checker.Add(this.Global.LineOfData > this.Global.LineOfField, "Global: line of field can't greater than line of data")
+	checker.Add(len(this.Elements) != 0, "element: element empty")
 
 	for _, itor := range this.Elements {
-		checker.Add(itor.ExcelName == "", "element: excelName empty")
-		checker.Add(itor.SheetName == "", "element: sheetName empty")
+		checker.Add(itor.ExcelName != "", "element: excelName empty")
+		checker.Add(itor.SheetName != "", "element: sheetName empty")
 	} // for
 
 	for _, itor := range checker.Errors() {
