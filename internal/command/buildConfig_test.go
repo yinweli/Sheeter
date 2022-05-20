cd0f145aeb741be4cf047ce1de63b877c30e2d7a
yinweli <yinweli@gmail.com>
2022-05-20 17:48:31 +0800

    修正單元測試

--- internal/command/buildConfig_test.go
+++ internal/command/buildConfig_test.go
@@ -1,71 +1,72 @@
 package command
 
 import (
+	"io/ioutil"
 	"testing"
 
+	"Sheeter/testdata"
+
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 func TestBuildConfig_Check(t *testing.T) {
-	buildConfig := BuildConfig{}
+	var buildConfig BuildConfig
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	assert.Equal(t, true, buildConfig.Check(), "check failed")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Global.ExcelPath = ""
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Global.ExcelPath)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Global.OutputPathJson = ""
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Global.OutputPathJson)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Global.OutputPathGo = ""
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Global.OutputPathGo)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Global.OutputPathCs = ""
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Global.OutputPathCs)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Global.OutputPathCpp = ""
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Global.OutputPathCpp)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Global.LineOfNote = 3
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Global.LineOfNote)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Global.LineOfField = 3
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Global.LineOfField)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Elements = []Element{}
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Elements empty)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Elements[0].ExcelName = ""
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Elements.ExcelName)")
 
-	recoveryBuildConfig(&buildConfig)
+	loadBuildConfig(t, &buildConfig)
 	buildConfig.Elements[0].SheetName = ""
 	assert.Equal(t, false, buildConfig.Check(), "check failed(Elements.SheetName)")
 
 }
 
-// recoveryBuildConfig 復原建立表格設定資料
-func recoveryBuildConfig(buildConfig *BuildConfig) {
-	buildConfig.Global.ExcelPath = "testme"
-	buildConfig.Global.OutputPathJson = "testme"
-	buildConfig.Global.OutputPathGo = "testme"
-	buildConfig.Global.OutputPathCs = "testme"
-	buildConfig.Global.OutputPathCpp = "testme"
-	buildConfig.Global.CppLibraryPath = "testme"
-	buildConfig.Global.LineOfNote = 0
-	buildConfig.Global.LineOfField = 1
-	buildConfig.Global.LineOfData = 2
-	buildConfig.Elements = []Element{
-		Element{ExcelName: "testme", SheetName: "testme"},
-	}
+// loadBuildConfig 讀取建立表格設定資料
+func loadBuildConfig(t *testing.T, buildConfig *BuildConfig) {
+	yamlPath := testdata.Path("test.yaml")
+	yamlFile, err := ioutil.ReadFile(yamlPath)
+
+	assert.NotNil(t, yamlFile, "load buildConfig failed")
+	assert.Nil(t, err, err)
+
+	err = yaml.Unmarshal(yamlFile, buildConfig)
+
+	assert.Nil(t, err, err)
 }
