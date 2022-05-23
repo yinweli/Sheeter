fded60728645a2ed154b28710581df7d3741bcd9
yinweli <yilin.li@dena.com>
2022-05-23 22:32:23 +0800

    新增64位元整數, 64位元整數陣列與單元測試

---
+++ internal/command/build/field/flieldLongArray.go
@@ -0,0 +1,50 @@
+package field
+
+import "Sheeter/internal/util"
+
+// LongArray 64位元整數陣列
+type LongArray struct {
+	Data
+}
+
+// TypeExcel 取得excel欄位類型
+func (this *LongArray) TypeExcel() string {
+	return "longArray"
+}
+
+// TypeCpp 取得c++欄位類型
+func (this *LongArray) TypeCpp() string {
+	return "std::vector<int64_t>"
+}
+
+// TypeCs 取得c#欄位類型
+func (this *LongArray) TypeCs() string {
+	return "List<long>"
+}
+
+// TypeGo 取得go欄位類型
+func (this *LongArray) TypeGo() string {
+	return "[]int64"
+}
+
+// Hide 是否隱藏
+func (this *LongArray) Hide() bool {
+	return false
+}
+
+// PrimaryKey 是否是主要索引
+func (this *LongArray) PrimaryKey() bool {
+	return false
+}
+
+// FillToMetas 寫入到元資料列表
+func (this *LongArray) FillToMetas(metas Metas, data string) error {
+	values, err := util.StringToInt64Array(data)
+
+	if err != nil {
+		return err
+	} // if
+
+	metas[this.Name] = values
+	return nil
+}
