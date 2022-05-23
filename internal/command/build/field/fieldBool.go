704ee49011fde196d655bd456302568ecf4f52e1
yinweli <yilin.li@dena.com>
2022-05-23 13:39:00 +0800

    新增布林值類型與單元測試

---
+++ internal/command/build/field/fieldBool.go
@@ -0,0 +1,50 @@
+package field
+
+import "strconv"
+
+// Bool 布林值
+type Bool struct {
+	Data
+}
+
+// TypeExcel 取得excel欄位類型
+func (this *Bool) TypeExcel() string {
+	return "bool"
+}
+
+// TypeCpp 取得c++欄位類型
+func (this *Bool) TypeCpp() string {
+	return "bool"
+}
+
+// TypeCs 取得c#欄位類型
+func (this *Bool) TypeCs() string {
+	return "bool"
+}
+
+// TypeGo 取得go欄位類型
+func (this *Bool) TypeGo() string {
+	return "bool"
+}
+
+// Hide 是否隱藏
+func (this *Bool) Hide() bool {
+	return false
+}
+
+// PrimaryKey 是否是主要索引
+func (this *Bool) PrimaryKey() bool {
+	return false
+}
+
+// FillToJsons 寫入到json列表
+func (this *Bool) FillToJsons(jsons Jsons, data string) error {
+	value, err := strconv.ParseBool(data)
+
+	if err != nil {
+		return err
+	} // if
+
+	jsons[this.Name] = value
+	return nil
+}
