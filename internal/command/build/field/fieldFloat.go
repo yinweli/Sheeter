1e57a194b6279d6893f33e7bd6ce6af38e6c3c93
yinweli <yinweli@gmail.com>
2022-05-23 17:36:06 +0800

    新增浮點數欄位與單元測試

---
+++ internal/command/build/field/fieldFloat.go
@@ -0,0 +1,50 @@
+package field
+
+import "strconv"
+
+// Float 布林值
+type Float struct {
+	Data
+}
+
+// TypeExcel 取得excel欄位類型
+func (this *Float) TypeExcel() string {
+	return "float"
+}
+
+// TypeCpp 取得c++欄位類型
+func (this *Float) TypeCpp() string {
+	return "float"
+}
+
+// TypeCs 取得c#欄位類型
+func (this *Float) TypeCs() string {
+	return "float"
+}
+
+// TypeGo 取得go欄位類型
+func (this *Float) TypeGo() string {
+	return "float32"
+}
+
+// Hide 是否隱藏
+func (this *Float) Hide() bool {
+	return false
+}
+
+// PrimaryKey 是否是主要索引
+func (this *Float) PrimaryKey() bool {
+	return false
+}
+
+// FillToMetas 寫入到元資料列表
+func (this *Float) FillToMetas(metas Metas, data string) error {
+	value, err := strconv.ParseFloat(data, 32)
+
+	if err != nil {
+		return err
+	} // if
+
+	metas[this.Name] = value
+	return nil
+}
---
+++ internal/command/build/field/fieldFloat_test.go
@@ -0,0 +1,28 @@
+package field
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFloat(t *testing.T) {
+	object := Float{
+		Data{
+			Raw:  "raw",
+			Note: "note",
+			Name: "name",
+		},
+	}
+	metas := Metas{}
+
+	assert.Equal(t, "float", object.TypeExcel(), "type excel failed")
+	assert.Equal(t, "float", object.TypeCpp(), "type cpp failed")
+	assert.Equal(t, "float", object.TypeCs(), "type cs failed")
+	assert.Equal(t, "float32", object.TypeGo(), "type go failed")
+	assert.Equal(t, false, object.Hide(), "hide failed")
+	assert.Equal(t, false, object.PrimaryKey(), "primary key failed")
+	assert.Nil(t, object.FillToMetas(metas, "0.999"), "fill to metas failed")
+	assert.InDelta(t, 0.999, metas[object.Name], 0.0001, "fill to metas failed")
+	assert.NotNil(t, object.FillToMetas(metas, "fake"), "fill to metas failed")
+}
