9ce4a774dcb9999ecde5141295708fa53f80e226
yinweli <yilin.li@dena.com>
2022-05-25 16:53:28 +0800

    更新執行資料

--- internal/command/build/cargo/cargo.go
+++ internal/command/build/cargo/cargo.go
@@ -12,10 +12,11 @@ type Cargo struct {
 	Progress *progressbar.ProgressBar // 進度條
 	Global   *config.Global           // 全域設定
 	Element  *config.Element          // 項目設定
-	Columns  []Column                 // 欄位列表
+	Fields   []field.Field            // 欄位列表
+	Columns  []Column                 // 行資料列表
 }
 
-// Column 欄位資料
+// Column 行資料
 type Column struct {
 	Note  string       // 欄位註解
 	Name  string       // 欄位名稱
