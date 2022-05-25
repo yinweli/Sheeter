9ac6a191f8cbc2f58483ff9f63032aad672c5ded
yinweli <yinweli@gmail.com>
2022-05-25 13:53:00 +0800

    新增工作目錄控制

--- testdata/testpath.go
+++ testdata/testpath.go
@@ -1,6 +1,7 @@
 package testdata
 
 import (
+	"os"
 	"path/filepath"
 	"runtime"
 )
@@ -13,18 +14,44 @@ const TestExcel = "test.xlsx"
 const TestSheet = "Data"
 
 func init() {
-	_, currentFile, _, ok := runtime.Caller(0)
+	_, file, _, ok := runtime.Caller(0)
 
 	if ok == false {
 		panic("can't get root")
 	}
 
-	rootPath = filepath.Dir(currentFile)
+	rootPath = filepath.Dir(file)
 }
 
 // Path 取得測試資料路徑
 func Path(path string) string {
 	return filepath.Join(rootPath, path)
+}
+
+// ChangeWorkDir 變更工作目錄到測試目錄
+func ChangeWorkDir() string {
+	dir, err := os.Getwd()
+
+	if err != nil {
+		panic(err)
+	} // if
+
+	err = os.Chdir(rootPath)
+
+	if err != nil {
+		panic(err)
+	} // if
+
+	return dir
+}
+
+// RestoreWorkDir 復原工作目錄
+func RestoreWorkDir(dir string) {
+	err := os.Chdir(dir)
+
+	if err != nil {
+		panic(err)
+	} // if
 }
 
 var rootPath string // 根路徑
