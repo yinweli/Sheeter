131b5daea4377c2782d461b2c093d8984805bfc8
yinweli <yilin.li@dena.com>
2022-05-25 18:30:58 +0800

    重構

--- internal/constant.go
+++ internal/constant.go
@@ -2,9 +2,11 @@ package internal
 
 const Title string = "sheeter"       // 程式名稱
 const Version string = "0.0.1"       // 版本字串
-const Separator string = ","         // 分隔字串
+const ArraySeparator string = ","    // 陣列分隔字串
+const FieldSeparator string = "#"    // 欄位分隔字串
 const Precision int = 6              // 小數點精度
 const Decimal int = 10               // 10進制
+const ProgressDefault int = 99999999 // 預設進度值
 const OutputPathJson string = "json" // 輸出路徑: json
 const OutputPathCpp string = "cpp"   // 輸出路徑: c++
 const OutputPathCs string = "cs"     // 輸出路徑: c#
