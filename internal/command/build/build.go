7a512be905bc75872afb2c8c5c001b72ad2b4186
yinweli <yinweli@gmail.com>
2022-05-25 14:05:30 +0800

    編譯命令製作中

--- internal/command/build/build.go
+++ internal/command/build/build.go
@@ -1,12 +1,9 @@
 package build
 
 import (
-	"io/ioutil"
-
-	"Sheeter/internal/command/build/config"
+	"Sheeter/internal/command/build/reader"
 
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
 )
 
 // Build 命令物件
@@ -20,48 +17,10 @@ var Build = &cobra.Command{
 
 // execute 執行命令
 func execute(cmd *cobra.Command, args []string) {
-	build := build{
-		cmd:  cmd,
-		args: args,
-	}
-
-	build.readConfig()
-
-	if build.err != nil {
-		cmd.Println(build.err)
-		return
-	} // if
-}
-
-// build 編譯命令
-type build struct {
-	cmd    *cobra.Command // 命令物件
-	args   []string       // 參數列表
-	err    error          // 錯誤物件
-	config *config.Config // 設定物件
-}
-
-// readConfig 讀取設定
-func (this *build) readConfig() {
-	bytes, err := ioutil.ReadFile(this.args[0])
+	_, err := reader.ReadConfig(args[0])
 
 	if err != nil {
-		this.err = err
-		return
-	} // if
-
-	this.config = &config.Config{}
-	err = yaml.Unmarshal(bytes, this.config)
-
-	if err != nil {
-		this.err = err
-		return
-	} // if
-
-	err = this.config.Check()
-
-	if err != nil {
-		this.err = err
+		cmd.Println(err)
 		return
 	} // if
 }
