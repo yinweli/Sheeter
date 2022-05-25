ca2b3bad45e64ba2e5eac8bed336e5f67944a376
yinweli <yinweli@gmail.com>
2022-05-25 16:39:29 +0800

    新增進度條產生工具

---
+++ internal/util/progress.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/schollz/progressbar/v3"
+)
+
+// NewProgressBar 建立進度條物件
+func NewProgressBar(max int, desc string, writer io.Writer) *progressbar.ProgressBar {
+	bar := progressbar.NewOptions(
+		max,
+		progressbar.OptionSetDescription(desc),
+		progressbar.OptionSetWriter(writer),
+		progressbar.OptionSetWidth(40),
+		progressbar.OptionThrottle(65*time.Millisecond),
+		progressbar.OptionShowCount(),
+		progressbar.OptionShowIts(),
+		progressbar.OptionOnCompletion(func() {
+			fmt.Printf("\r")
+		}),
+		progressbar.OptionSpinnerType(14),
+	)
+	_ = bar.RenderBlank()
+
+	return bar
+}
