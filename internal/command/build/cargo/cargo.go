8c2081f9bbef73c733d409db8e23f6bd84378e4c
yinweli <yilin.li@dena.com>
2022-05-25 16:46:51 +0800

    新增執行資料

---
+++ internal/command/build/cargo/cargo.go
@@ -0,0 +1,24 @@
+package cargo
+
+import (
+	"Sheeter/internal/command/build/config"
+	"Sheeter/internal/command/build/field"
+
+	"github.com/schollz/progressbar/v3"
+)
+
+// Cargo 執行資料
+type Cargo struct {
+	Progress *progressbar.ProgressBar // 進度條
+	Global   *config.Global           // 全域設定
+	Element  *config.Element          // 項目設定
+	Columns  []Column                 // 欄位列表
+}
+
+// Column 欄位資料
+type Column struct {
+	Note  string       // 欄位註解
+	Name  string       // 欄位名稱
+	Field *field.Field // 欄位類型
+	Datas []string     // 資料列表
+}
