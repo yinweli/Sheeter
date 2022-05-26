39dfcdfd3ce4c90659085ee6584f62924e2161bb
yinweli <yilin.li@dena.com>
2022-05-26 18:23:07 +0800

    重構

--- internal/util/progress_test.go
+++ internal/util/progress_test.go
@@ -4,9 +4,18 @@ import (
 	"os"
 	"testing"
 
+	"Sheeter/internal"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewProgressBar(t *testing.T) {
-	assert.NotNil(t, NewProgressBar(10, "desc", os.Stdout))
+	progress := NewProgressBar(10, "desc", os.Stdout)
+
+	assert.NotNil(t, progress)
+	assert.Nil(t, progress.Finish())
+}
+
+func TestCalcProgress(t *testing.T) {
+	assert.Equal(t, int(float32(5*7)*internal.ProgressFactor), CalcProgress(5, 7))
 }
