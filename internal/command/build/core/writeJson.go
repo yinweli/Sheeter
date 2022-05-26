d15f688091ffda1ddeee315f80cd455ee22ae6ac
yinweli <yilin.li@dena.com>
2022-05-26 17:25:13 +0800

    寫入Json製作中

--- internal/command/build/core/writeJson.go
+++ internal/command/build/core/writeJson.go
@@ -1,6 +1,33 @@
 package core
 
+import "fmt"
+
 // WriteJson 寫入json
 func WriteJson(task *Task) error {
 	return nil
 }
+
+// buildJBoxes 建立json箱列表
+func buildJBoxes(task *Task) (jboxes []jbox, err error) {
+	for _, itor := range task.Columns {
+		for row, data := range itor.Datas {
+			result, err := itor.Field.Transform(data)
+
+			if err != nil {
+				return nil, fmt.Errorf("convert to json failed: %s [%s(%d) : %s]", task.Element.GetFullName(), itor.Name, row, err)
+			} // if
+
+			if len(jboxes) <= row {
+				jboxes = append(jboxes, jbox{})
+			} // if
+
+			jboxes[row][itor.Name] = result
+			_ = task.Progress.Add(1)
+		} // for
+	} // for
+
+	return jboxes, nil
+}
+
+// jbox json箱
+type jbox map[string]interface{}
