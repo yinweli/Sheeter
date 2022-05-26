93b7f800710ac5d96b9527b7db6baf3b1e2af711
yinweli <yilin.li@dena.com>
2022-05-26 09:38:38 +0800

    新增常數

--- internal/constant.go
+++ internal/constant.go
@@ -1,13 +1,14 @@
 package internal
 
-const Title string = "sheeter"       // 程式名稱
-const Version string = "0.0.1"       // 版本字串
-const ArraySeparator string = ","    // 陣列分隔字串
-const FieldSeparator string = "#"    // 欄位分隔字串
-const Precision int = 6              // 小數點精度
-const Decimal int = 10               // 10進制
-const ProgressDefault int = 99999999 // 預設進度值
-const OutputPathJson string = "json" // 輸出路徑: json
-const OutputPathCpp string = "cpp"   // 輸出路徑: c++
-const OutputPathCs string = "cs"     // 輸出路徑: c#
-const OutputPathGo string = "go"     // 輸出路徑: go
+const Title string = "sheeter"         // 程式名稱
+const Version string = "0.0.1"         // 版本字串
+const ArraySeparator string = ","      // 陣列分隔字串
+const FieldSeparator string = "#"      // 欄位分隔字串
+const Precision int = 6                // 小數點精度
+const Decimal int = 10                 // 10進制
+const ProgressDefault int64 = 99999999 // 預設進度值
+const TaskMax int = 5                  // 工作數量, 為輸出工作數量+讀取工作數量
+const OutputPathJson string = "json"   // 輸出路徑: json
+const OutputPathCpp string = "cpp"     // 輸出路徑: c++
+const OutputPathCs string = "cs"       // 輸出路徑: c#
+const OutputPathGo string = "go"       // 輸出路徑: go
