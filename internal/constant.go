893e0d45fe5e01996903847c05a740fd75b51575
yinweli <yinweli@gmail.com>
2022-05-27 09:35:45 +0800

    重構

--- internal/constant.go
+++ internal/constant.go
@@ -1,14 +1,12 @@
 package internal
 
-const Title string = "sheeter"         // 程式名稱
-const Version string = "0.0.1"         // 版本字串
-const ArraySeparator string = ","      // 陣列分隔字串
-const FieldSeparator string = "#"      // 欄位分隔字串
-const Precision int = 6                // 小數點精度
-const Decimal int = 10                 // 10進制
-const ProgressDefault int64 = 99999999 // 預設進度值
-const ProgressFactor float32 = 1.2     // 進度值因子
-const OutputPathJson string = "json"   // 輸出路徑: json
-const OutputPathCpp string = "cpp"     // 輸出路徑: c++
-const OutputPathCs string = "cs"       // 輸出路徑: c#
-const OutputPathGo string = "go"       // 輸出路徑: go
+const Title string = "sheeter"       // 程式名稱
+const Version string = "0.0.1"       // 版本字串
+const ArraySeparator string = ","    // 陣列分隔字串
+const FieldSeparator string = "#"    // 欄位分隔字串
+const Precision int = 6              // 小數點精度
+const Decimal int = 10               // 10進制
+const OutputPathJson string = "json" // 輸出路徑: json
+const OutputPathCpp string = "cpp"   // 輸出路徑: c++
+const OutputPathCs string = "cs"     // 輸出路徑: c#
+const OutputPathGo string = "go"     // 輸出路徑: go
