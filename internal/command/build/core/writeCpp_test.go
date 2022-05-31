fab0a04635dcaca70d3ce5b5ddf753b7ba309ffd
yinweli <yinweli@gmail.com>
2022-05-31 16:11:04 +0800

    修正單元測試

--- internal/command/build/core/writeCpp_test.go
+++ internal/command/build/core/writeCpp_test.go
@@ -7,20 +7,36 @@ import (
 	"testing"
 
 	"Sheeter/internal/util"
+	"Sheeter/testdata"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestWriteCpp(t *testing.T) {
+	dir := testdata.ChangeWorkDir()
+	defer testdata.RestoreWorkDir(dir)
+
 	cargo := mockWriteCppCargo()
 	path, err := WriteCpp(cargo)
 	assert.Nil(t, err)
 	assert.Equal(t, filepath.Join(OutputPathCpp, "realData.hpp"), path)
 	assert.FileExists(t, path)
 
-	cargo = mockWriteCppCargo()
+	bytes, err := ioutil.ReadFile(path)
+	assert.Nil(t, err)
+	assert.Equal(t, mockWriteCppString(), string(bytes[:]))
+
+	err = os.RemoveAll(OutputPathCpp)
+	assert.Nil(t, err)
+}
+
+func TestWriteCppFailed(t *testing.T) {
+	dir := testdata.ChangeWorkDir()
+	defer testdata.RestoreWorkDir(dir)
+
+	cargo := mockWriteCppCargo()
 	cargo.Global = nil
-	path, err = WriteCpp(cargo)
+	_, err := WriteCpp(cargo)
 	assert.NotNil(t, err)
 
 	err = os.RemoveAll(OutputPathCpp)
@@ -39,8 +55,60 @@ func mockWriteCppCargo() *Cargo {
 		},
 		Columns: []*Column{
 			{Note: "note0", Name: "name0", Field: &FieldInt{}},
-			{Note: "note1", Name: "name1", Field: &FieldInt{}},
-			{Note: "note2", Name: "name2", Field: &FieldInt{}},
+			{Note: "note1", Name: "name1", Field: &FieldBool{}},
+			{Note: "note2", Name: "name2", Field: &FieldText{}},
 		},
 	}
+}
+
+func mockWriteCppString() string {
+	return `// generation by sheeter ^o<
+// use nlohmann json library
+// github: https://github.com/nlohmann/json
+
+#pragma once
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+
+namespace Sheeter {
+using nlohmann::json;
+
+#ifndef PKEY
+#define PKEY
+using pkey = int32_t;
+#endif // !PKEY
+
+struct RealData { 
+    int32_t Name0; // note0
+    bool Name1; // note1
+    std::string Name2; // note2
+
+    static std::string get_filename() {
+        return "realData.json"
+    }
+};
+
+inline json get_untyped(const json& j, const char* property) {
+    return j.find(property) != j.end() ? j.at(property).get<json>() : json();
+}
+} // namespace Sheeter
+
+namespace nlohmann {
+inline void from_json(const json& _j, struct Sheeter::RealData& _x) { 
+    _x.Name0 = _j.at("Name0").get<int32_t>();
+    _x.Name1 = _j.at("Name1").get<bool>();
+    _x.Name2 = _j.at("Name2").get<std::string>();
+}
+
+inline void to_json(json& _j, const struct Sheeter::RealData& _x) { 
+    _j = json::object();
+    _j["Name0"] = _x.Name0;
+    _j["Name1"] = _x.Name1;
+    _j["Name2"] = _x.Name2;
+}
+} // namespace nlohmann`
 }
