27cd2906e930c55710fce2d4a0bc3cfa1984ca69
yinweli <yinweli@gmail.com>
2022-06-06 18:57:54 +0800

    更新README.md

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ sheeter build xxxx.yaml
 
 # TODO
 * 浮點數轉換由strconv.ParseFloat改用strconv.FormatFloat
-* 浮點數精度改由設定檔指定
+* 浮點數精度改由設定檔指定或是在欄位上指定
 * Jobs機制放棄介面, 改用結構加上函式指標, 應該會比較單純
 * 加上writeProto格式
 * 事前檢查機制(可能要加到Jobs中), 要檢查是否有安裝Go/Protoc
--- cmd/sheeter.go
+++ cmd/sheeter.go
@@ -21,7 +21,7 @@ func main() {
 }
 
 // TODO: 浮點數轉換由strconv.ParseFloat改用strconv.FormatFloat
-// TODO: 浮點數精度改由設定檔指定
+// TODO: 浮點數精度改由設定檔指定或是在欄位上指定
 // TODO: Jobs機制放棄介面, 改用結構加上函式指標, 應該會比較單純
 // TODO: 加上writeProto格式
 // TODO: 事前檢查機制(可能要加到Jobs中), 要檢查是否有安裝Go/Protoc
