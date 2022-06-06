f3d1e2825b082cc2165e29d546f4a611e52dc580
yinweli <yinweli@gmail.com>
2022-06-06 21:38:04 +0800

    更新README.md

--- README.md
+++ README.md
@@ -37,3 +37,4 @@ sheeter build xxxx.yaml
 * Jobs機制放棄介面, 改用結構加上函式指標, 應該會比較單純
 * 加上writeProto格式
 * 事前檢查機制(可能要加到Jobs中), 要檢查是否有安裝Go/Protoc
+* 讓BOM機制有效
--- cmd/sheeter.go
+++ cmd/sheeter.go
@@ -25,3 +25,4 @@ func main() {
 // TODO: Jobs機制放棄介面, 改用結構加上函式指標, 應該會比較單純
 // TODO: 加上writeProto格式
 // TODO: 事前檢查機制(可能要加到Jobs中), 要檢查是否有安裝Go/Protoc
+// TODO: 讓BOM機制有效
