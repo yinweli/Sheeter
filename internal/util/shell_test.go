cc07c763a3fea3aaedfb81162fa0e624e2c94e25
yinweli <yinweli@gmail.com>
2022-06-21 17:45:39 +0800

    新增單元測試

--- internal/util/shell_test.go
+++ internal/util/shell_test.go
@@ -1,1 +1,18 @@
 package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShellRun(t *testing.T) {
+	err, detail := ShellRun("echo", "value")
+	assert.NotNil(t, err)
+	assert.Equal(t, "", detail)
+}
+
+func TestShellExist(t *testing.T) {
+	assert.Nil(t, ShellExist("go"))
+	assert.NotNil(t, ShellExist("x"))
+}
