93ac36bdc4ce24c77b9af8b4db68758b57b891cb
yinweli <yinweli@gmail.com>
2022-07-23 09:28:20 +0800

    重構

--- internal/command/build/core/layer.go
+++ internal/command/build/core/layer.go
@@ -10,30 +10,30 @@ const (
 	LayerArray         // 陣列階層
 )
 
-// ParseLayer 解析字串為階層, 格式為 {name 或 {[]name 或 }
-func ParseLayer(input string) (layer []Layer, back int, err error) {
-	const separateArray = "{[]" // 階層字串以'{[]'符號開始, 表示為陣列的開始
-	const separateStruct = "{"  // 階層字串以'{'符號開始, 表示為結構的開始
-	const separateEnd = "}"     // 階層字串以'}'符號開始, 表示為結構/陣列的結束
+const tokenArray = "{[]" // 階層字串以'{[]'符號開始, 表示為陣列的開始
+const tokenStruct = "{"  // 階層字串以'{'符號開始, 表示為結構的開始
+const tokenEnd = "}"     // 階層字串以'}'符號開始, 表示為結構/陣列的結束
 
+// ParseLayer 解析字串為階層, 格式為 {name 或 {[]name 或 }
+func ParseLayer(input string) (layer []Layer, back int, err error) { // TODO: layerParse還沒做玩
 	tokens := strings.Fields(input)
 	mode := true // 階層模式, true表示是開始模式, false是結束模式; 結束模式下就不可以碰到陣列開始/結構開始, 會視為錯誤
 
 	for _, itor := range tokens {
 		switch {
-		case mode && strings.HasPrefix(itor, separateArray):
+		case mode && strings.HasPrefix(itor, tokenArray):
 			layer = append(layer, Layer{
-				Name: strings.TrimPrefix(itor, separateArray),
+				Name: strings.TrimPrefix(itor, tokenArray),
 				Type: LayerArray,
 			})
 
-		case mode && strings.HasPrefix(itor, separateStruct):
+		case mode && strings.HasPrefix(itor, tokenStruct):
 			layer = append(layer, Layer{
-				Name: strings.TrimPrefix(itor, separateStruct),
+				Name: strings.TrimPrefix(itor, tokenStruct),
 				Type: LayerStruct,
 			})
 
-		case strings.HasPrefix(itor, separateEnd):
+		case strings.HasPrefix(itor, tokenEnd):
 			mode = false
 			back++
 
--- internal/util/convert.go
+++ internal/util/convert.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-const arraySeparator = "," // 陣列分隔字串
+const separateArray = "," // 陣列分隔字串
 
 // StrToBool 字串轉布林值
 func StrToBool(input string) (result bool, err error) {
@@ -21,7 +21,7 @@ func StrToBool(input string) (result bool, err error) {
 
 // StrToBoolArray 字串轉布林值陣列
 func StrToBoolArray(input string) (results []bool, err error) {
-	for _, itor := range strings.Split(input, arraySeparator) {
+	for _, itor := range strings.Split(input, separateArray) {
 		result, err := StrToBool(itor)
 
 		if err != nil {
@@ -47,7 +47,7 @@ func StrToInt(input string) (result int64, err error) {
 
 // StrToIntArray 字串轉整數陣列
 func StrToIntArray(input string) (results []int64, err error) {
-	for _, itor := range strings.Split(input, arraySeparator) {
+	for _, itor := range strings.Split(input, separateArray) {
 		result, err := StrToInt(itor)
 
 		if err != nil {
@@ -73,7 +73,7 @@ func StrToFloat(input string) (result float64, err error) {
 
 // StrToFloatArray 字串轉浮點數陣列
 func StrToFloatArray(input string) (results []float64, err error) {
-	for _, itor := range strings.Split(input, arraySeparator) {
+	for _, itor := range strings.Split(input, separateArray) {
 		result, err := StrToFloat(itor)
 
 		if err != nil {
@@ -88,5 +88,5 @@ func StrToFloatArray(input string) (results []float64, err error) {
 
 // StrToStrArray 字串轉為字串陣列
 func StrToStrArray(input string) []string {
-	return strings.Split(input, arraySeparator)
+	return strings.Split(input, separateArray)
 }
