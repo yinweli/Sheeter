626298fd1238a081b53e06b0c395f3c57461026b
yinweli <yinweli@gmail.com>
2022-07-23 10:17:35 +0800

    TestDuplicate

--- internal/util/duplicate_test.go
+++ internal/util/duplicate_test.go
@@ -3,15 +3,46 @@ package util
 import (
 	"testing"
 
+	"github.com/emirpasic/gods/sets/hashset"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestDuplicate(t *testing.T) {
-	duplicate := Duplicate{}
-	assert.True(t, duplicate.Check("001"))
-	assert.True(t, duplicate.Check("001/002"))
-	assert.True(t, duplicate.Check("001/002/003"))
-	assert.False(t, duplicate.Check("001"))
-	assert.False(t, duplicate.Check("001/002"))
-	assert.False(t, duplicate.Check("001/002/003"))
+
+	suite.Run(t, new(SuiteDuplicate))
+}
+
+type SuiteDuplicate struct {
+	suite.Suite
+	item1 string
+	item2 string
+	item3 string
+}
+
+func (this *SuiteDuplicate) SetupSuite() {
+	this.item1 = "001"
+	this.item2 = "001/002"
+	this.item3 = "001/002/003"
+}
+
+func (this *SuiteDuplicate) target() *Duplicate {
+	return &Duplicate{
+		datas: hashset.New(),
+	}
+}
+
+func (this *SuiteDuplicate) TestDuplicate() {
+	target := this.target()
+
+	assert.True(this.T(), target.Check(this.item1))
+	assert.True(this.T(), target.Check(this.item2))
+	assert.True(this.T(), target.Check(this.item3))
+	assert.False(this.T(), target.Check(this.item1))
+	assert.False(this.T(), target.Check(this.item2))
+	assert.False(this.T(), target.Check(this.item3))
+}
+
+func (this *SuiteDuplicate) TestNewDuplicate() {
+	assert.NotNil(this.T(), NewDuplicate())
 }
