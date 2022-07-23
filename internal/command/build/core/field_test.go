92899b225805148a86b680af165eadaca59343ac
yinweli <yinweli@gmail.com>
2022-07-23 18:41:47 +0800

    TestField

--- internal/command/build/core/field_test.go
+++ internal/command/build/core/field_test.go
@@ -4,17 +4,26 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestParseField(t *testing.T) {
+func TestField(t *testing.T) {
+	suite.Run(t, new(SuiteField))
+}
+
+type SuiteField struct {
+	suite.Suite
+}
+
+func (this *SuiteField) TestParseField() {
 	name, field, err := ParseField("real#bool")
-	assert.Nil(t, err)
-	assert.Equal(t, "real", name)
-	assert.Equal(t, (&FieldBool{}).Type(), field.Type())
+	assert.Nil(this.T(), err)
+	assert.Equal(this.T(), "real", name)
+	assert.Equal(this.T(), (&FieldBool{}).Type(), field.Type())
 	_, _, err = ParseField("fa-ke#fake")
-	assert.NotNil(t, err)
+	assert.NotNil(this.T(), err)
 	_, _, err = ParseField("fake#fake")
-	assert.NotNil(t, err)
+	assert.NotNil(this.T(), err)
 	_, _, err = ParseField("unknown")
-	assert.NotNil(t, err)
+	assert.NotNil(this.T(), err)
 }
