4f4dbf7c86f0a5e10a3b5300173255d7069b798c
yinweli <yinweli@gmail.com>
2022-07-23 09:16:50 +0800

    fix lint

--- internal/command/build/core/layer.go
+++ internal/command/build/core/layer.go
@@ -3,8 +3,6 @@ package core
 import (
 	"fmt"
 	"strings"
-
-	"github.com/yinweli/Sheeter/internal/util"
 )
 
 const (
@@ -19,52 +17,32 @@ func ParseLayer(input string) (layer []Layer, back int, err error) {
 	const separateEnd = "}"     // 階層字串以'}'符號開始, 表示為結構/陣列的結束
 
 	tokens := strings.Fields(input)
-	forward := true
+	mode := true // 階層模式, true表示是開始模式, false是結束模式; 結束模式下就不可以碰到陣列開始/結構開始, 會視為錯誤
 
 	for _, itor := range tokens {
-		if forward {
-			switch {
-			case strings.HasPrefix(itor, separateArray):
-				layer = append(layer, Layer{
-					Name: strings.TrimPrefix(itor, separateArray),
-					Type: LayerArray,
-				})
+		switch {
+		case mode && strings.HasPrefix(itor, separateArray):
+			layer = append(layer, Layer{
+				Name: strings.TrimPrefix(itor, separateArray),
+				Type: LayerArray,
+			})
 
-			case strings.HasPrefix(itor, separateStruct):
-				layer = append(layer, Layer{
-					Name: strings.TrimPrefix(itor, separateStruct),
-					Type: LayerStruct,
-				})
+		case mode && strings.HasPrefix(itor, separateStruct):
+			layer = append(layer, Layer{
+				Name: strings.TrimPrefix(itor, separateStruct),
+				Type: LayerStruct,
+			})
 
-			case strings.HasPrefix(itor, separateEnd):
-				forward = false
-				back++
+		case strings.HasPrefix(itor, separateEnd):
+			mode = false
+			back++
 
-			default:
-				return nil, 0, fmt.Errorf("layer format failed: %s", input)
-			} // switch
-		} else {
-
-		} // if
+		default:
+			return nil, 0, fmt.Errorf("layer format failed: %s", input)
+		} // switch
 	} // for
 
-	before, after, ok := strings.Cut(input, "#") // 欄位字串以'#'符號分割為名稱與欄位
-
-	if ok == false {
-		return "", nil, fmt.Errorf("field format failed: %s", input)
-	} // if
-
-	if util.VariableCheck(before) == false {
-		return "", nil, fmt.Errorf("name invalid: %s", input)
-	} // if
-
-	for _, itor := range fields {
-		if itor.Type() == after {
-			return before, itor, nil
-		} // if
-	} // for
-
-	return "", nil, fmt.Errorf("field not found: %s", input)
+	return layer, back, nil
 }
 
 // Layer 階層資料
