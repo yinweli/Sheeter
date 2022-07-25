1c96aa5afd6cca15723ad81468304214fae171a8
yinweli <yinweli@gmail.com>
2022-07-25 20:30:11 +0800

    TestFieldText

--- internal/command/build/core/fieldText_test.go
+++ internal/command/build/core/fieldText_test.go
@@ -4,24 +4,49 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestFieldText(t *testing.T) {
-	field := mockFieldText()
-	assert.Equal(t, "text", field.Type())
-	assert.Equal(t, true, field.IsShow())
-	assert.Equal(t, false, field.IsPkey())
-	assert.Equal(t, "", field.ToJsonDefault())
-
-	result, err := field.ToJsonValue("ball,book,pack")
-	assert.Nil(t, err)
-	assert.Equal(t, "ball,book,pack", result)
-
-	result, err = field.ToLuaValue("ball,book,pack")
-	assert.Nil(t, err)
-	assert.Equal(t, "\"ball,book,pack\"", result)
+	suite.Run(t, new(SuiteFieldText))
 }
 
-func mockFieldText() *FieldText {
+type SuiteFieldText struct {
+	suite.Suite
+}
+
+func (this *SuiteFieldText) target() *FieldText {
 	return &FieldText{}
+}
+
+func (this *SuiteFieldText) TestType() {
+	assert.Equal(this.T(), "text", this.target().Type())
+}
+
+func (this *SuiteFieldText) TestIsShow() {
+	assert.Equal(this.T(), true, this.target().IsShow())
+}
+
+func (this *SuiteFieldText) TestIsPkey() {
+	assert.Equal(this.T(), false, this.target().IsPkey())
+}
+
+func (this *SuiteFieldText) TestToJsonDefault() {
+	assert.Equal(this.T(), "", this.target().ToJsonDefault())
+}
+
+func (this *SuiteFieldText) TestToJsonValue() {
+	target := this.target()
+
+	result, err := target.ToJsonValue("ball,book,pack")
+	assert.Nil(this.T(), err)
+	assert.Equal(this.T(), "ball,book,pack", result)
+}
+
+func (this *SuiteFieldText) TestToLuaValue() {
+	target := this.target()
+
+	result, err := target.ToLuaValue("ball,book,pack")
+	assert.Nil(this.T(), err)
+	assert.Equal(this.T(), "\"ball,book,pack\"", result)
 }
