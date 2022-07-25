b31fc1ed0990e747dfd4ae8de9d1633f91a652fa
yinweli <yinweli@gmail.com>
2022-07-25 20:20:24 +0800

    TestFieldInt

--- internal/command/build/core/fieldInt_test.go
+++ internal/command/build/core/fieldInt_test.go
@@ -4,29 +4,55 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/yinweli/Sheeter/testdata"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestFieldInt(t *testing.T) {
-	field := mockFieldInt()
-	assert.Equal(t, "int", field.Type())
-	assert.Equal(t, true, field.IsShow())
-	assert.Equal(t, false, field.IsPkey())
-	assert.Equal(t, int64(0), field.ToJsonDefault())
-
-	result, err := field.ToJsonValue("123456789")
-	assert.Nil(t, err)
-	assert.Equal(t, int64(123456789), result)
-	_, err = field.ToJsonValue(testdata.UnknownStr)
-	assert.NotNil(t, err)
-
-	result, err = field.ToLuaValue("123456789")
-	assert.Nil(t, err)
-	assert.Equal(t, "123456789", result)
-	_, err = field.ToLuaValue(testdata.UnknownStr)
-	assert.NotNil(t, err)
+	suite.Run(t, new(SuiteFieldInt))
 }
 
-func mockFieldInt() *FieldInt {
+type SuiteFieldInt struct {
+	suite.Suite
+}
+
+func (this *SuiteFieldInt) target() *FieldInt {
 	return &FieldInt{}
+}
+
+func (this *SuiteFieldInt) TestType() {
+	assert.Equal(this.T(), "int", this.target().Type())
+}
+
+func (this *SuiteFieldInt) TestIsShow() {
+	assert.Equal(this.T(), true, this.target().IsShow())
+}
+
+func (this *SuiteFieldInt) TestIsPkey() {
+	assert.Equal(this.T(), false, this.target().IsPkey())
+}
+
+func (this *SuiteFieldInt) TestToJsonDefault() {
+	assert.Equal(this.T(), int64(0), this.target().ToJsonDefault())
+}
+
+func (this *SuiteFieldInt) TestToJsonValue() {
+	target := this.target()
+
+	result, err := target.ToJsonValue("123456789")
+	assert.Nil(this.T(), err)
+	assert.Equal(this.T(), int64(123456789), result)
+
+	_, err = target.ToJsonValue("?????")
+	assert.NotNil(this.T(), err)
+}
+
+func (this *SuiteFieldInt) TestToLuaValue() {
+	target := this.target()
+
+	result, err := target.ToLuaValue("123456789")
+	assert.Nil(this.T(), err)
+	assert.Equal(this.T(), "123456789", result)
+
+	_, err = target.ToLuaValue("?????")
+	assert.NotNil(this.T(), err)
 }
