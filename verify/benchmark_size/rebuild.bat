9827d72139952427d71df079ada7ebefe11ce075
yinweli <yinweli@gmail.com>
2022-09-29 17:48:56 +0800

    [feature] 更新驗證資料

---
+++ verify/benchmark_size/rebuild.bat
@@ -0,0 +1,11 @@
+rmdir /s /q json
+rmdir /s /q proto
+rmdir /s /q template
+del /f protoCs.bat
+del /f protoCs.sh
+del /f protoGo.bat
+del /f protoGo.sh
+.\sheeter.exe build --config benchmark.yaml
+call protoCs.bat
+call protoGo.bat
+del /f sheeter.exe
