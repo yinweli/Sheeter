809b822466ae3d829898dccabd84ace8483a8427
yinweli <yinweli@gmail.com>
2022-10-21 18:02:09 +0800

    [feature] 更新測試資料

--- support/benchmark_size/rebuild.bat
+++ support/benchmark_size/rebuild.bat
@@ -4,4 +4,7 @@ rmdir /s /q proto
 rmdir /s /q template
 7z e -y -osrc benchmark_size.7z
 .\sheeter.exe build --config benchmark.yaml
+dotnet csharpier .
+gofmt -w .
+buf format -w .
 del /f sheeter.exe
